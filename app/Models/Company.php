ca54604628e91f14f25b06d447e5770c00cfdeb1
VitalyZibulski <laravel.vitaly.zibulski2017@gmail.com>
2023-07-11 16:21:20 +0200

    manage activities

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Company extends Model
 {
@@ -11,8 +12,13 @@ class Company extends Model
 
     protected $fillable = ['name'];
 
-    public function users()
+    public function users(): HasMany
     {
         return $this->hasMany(User::class);
+    }
+
+    public function activities(): HasMany
+    {
+        return $this->hasMany(Activity::class);
     }
 }
